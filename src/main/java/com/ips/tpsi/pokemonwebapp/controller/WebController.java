8238a311a5e1208cdf67b7e042029f62d87b5373
Danilo <220001205@esg.ipsantarem.pt>
2023-11-28 17:54:11 +0000

    criacao de mappings

---
+++ src/main/java/com/ips/tpsi/pokemonwebapp/controller/WebController.java
@@ -0,0 +1,22 @@
+package com.ips.tpsi.pokemonwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+    @GetMapping("/home")
+    public ModelAndView getHome() {
+        ModelAndView mv = new ModelAndView("home.html");
+        return mv;
+    }
+
+    @GetMapping("/name")
+    public ModelAndView getName() {
+        ModelAndView mv = new ModelAndView("index.html");
+
+        mv.addObject("nome", "danilo");
+        return mv;
+    }
+}
