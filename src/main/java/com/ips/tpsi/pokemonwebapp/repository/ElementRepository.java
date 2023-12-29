d4afdd606f6ac85d63e31a26f9dca11c7ba41d09
Tiago <>
2023-12-29 21:10:06 +0000

    Adicionar query para procura por nome

--- src/main/java/com/ips/tpsi/pokemonwebapp/repository/ElementRepository.java
+++ src/main/java/com/ips/tpsi/pokemonwebapp/repository/ElementRepository.java
@@ -3,10 +3,15 @@ package com.ips.tpsi.pokemonwebapp.repository;
 import com.ips.tpsi.pokemonwebapp.entity.Element;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ElementRepository extends JpaRepository<Element, Integer> {
 
-
+    @Query("SELECT e FROM Element e WHERE e.elementDesc = :elementDesc")
+    Element findbyName(String elementDesc);
 }
