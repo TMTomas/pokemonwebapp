e398dabf7525e29e06c80e95a45878e63a4a98c2
Tiago <>
2023-12-29 21:11:26 +0000

    Adicionar opção de create e delete

--- src/main/java/com/ips/tpsi/pokemonwebapp/controller/ElementController.java
+++ src/main/java/com/ips/tpsi/pokemonwebapp/controller/ElementController.java
@@ -2,11 +2,11 @@ package com.ips.tpsi.pokemonwebapp.controller;
 
 import com.ips.tpsi.pokemonwebapp.bc.ElementService;
 import com.ips.tpsi.pokemonwebapp.bc.PokemonService;
+import com.ips.tpsi.pokemonwebapp.entity.Element;
+import com.ips.tpsi.pokemonwebapp.entity.Pokemon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -41,4 +41,13 @@ public class ElementController {
         return mv;
     }
 
+    @PostMapping("/consulta/elements/create")
+    public Element createElement(@RequestBody Element element){
+        return elementBC.saveElement(element);
+    }
+
+    @DeleteMapping("consulta/elements/{idElement}")
+    public void deletePokemon(@PathVariable("element") Integer idElement) {
+        elementBC.deleteElement(idElement);
+    }
 }
